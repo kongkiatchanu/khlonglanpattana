019f7dfc8923c60b120fa916c445e596825681f0
signoutz <aunkaffpom@gmail.com>
2023-12-07 20:09:15 +0700

    uploads

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+    $title = 'สำนักงานเทศบาลตำบลคลองลานพัฒนา by CCDC';
+    $icon='logo_khlonglanpattana.png';
+    $icon_title='logo_khlonglanpattana.ico';
+    $contact_title = 'สำนักงานเทศบาลตำบลคลองลานพัฒนา';
+    $contact_detail = 'ที่อยู่ 999 ตำบลคลองลานพัฒนา อำเภอคลองลาน จังหวัดกำแพงเพชร 62180
+    <br>โทรศัพท์ : 0-5573-5184
+    <br>E-mail : saraban@khlonglanpattana.go.th';
+    $facebook = 'https://www.facebook.com/100064590946233';
+    $web = 'https://www.khlonglanpattana.go.th/';
+    $footer = 'Copyright &copy; 2020. All rights reserved by CCDC.';
+    // $footer_repoting = 'Copyright &copy; 2020. All rights reserved by CCDC.';
+?>
